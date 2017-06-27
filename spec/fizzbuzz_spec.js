c72af292bea7bfd5f423415460af8508ae57c73f
faraznaeem <faraznaeem@gmail.com>
2017-06-27 12:47:42 +0200

    downloaded Specrunner

--- spec/fizzbuzz_spec.js
+++ spec/fizzbuzz_spec.js
@@ -17,8 +17,9 @@ describe("fizzBuzz", function() {
   it("#3 fizzbuzz should return fizzbuzz if div by 3 & 5", function() {
     expect(subject.fizzBuzzFunction(15)).toBe('FizzBuzz!');
   });
+
   it("#4 fizzbuzz should return integer if not div by 3 & 5", function() {
     expect(subject.fizzBuzzFunction(2)).toEqual(2);
-  })
+  });
 
 });
