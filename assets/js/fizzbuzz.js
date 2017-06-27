8955e3f8a3a66da947d318a7cf2c8a5816b388f1
faraznaeem <faraznaeem@gmail.com>
2017-06-27 12:22:30 +0200

    changed position on first and third if statement

--- assets/js/fizzbuzz.js
+++ assets/js/fizzbuzz.js
@@ -1,15 +1,15 @@
 function fizzBuzz(){
-};
+}
 
 fizzBuzz.prototype.fizzBuzzFunction = function(integer){
-  if(integer % 3 === 0){
-    return ("fizz");
+  if(integer % 3 === 0 && integer % 5 === 0){
+    return ("FizzBuzz!");
   }
   else if (integer % 5 === 0) {
     return ("buzz");
   }
-  else if (integer % 3 === 0 || integer % 5 === 0) {
-    return ("FizzBuzz!");
+  else if (integer % 3 === 0) {
+    return ("fizz");
   }
-  
+
 };
